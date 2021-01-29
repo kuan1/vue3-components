9d7bf5d255a0c8551c291659759dca6718a68548
luzhongkuan <luzhongk@126.com>
2021-01-30 02:45:56 +0800

    chore: test

---
+++ packages/pro-layout/src/pro-layout/defaultSettings.ts
@@ -0,0 +1,16 @@
+const defaultSettings = {
+  navTheme: 'dark',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: false,
+  menu: {
+    locale: true,
+  },
+  headerHeight: 48,
+  title: 'Ant Design Pro',
+  iconfontUrl: '',
+  primaryColor: '#1890ff',
+}
+
+export default defaultSettings
